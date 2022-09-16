32c63ed45871754f7e6a75d2c40cc9e987ee9f0b
emanet1 <enamejewaemmanuel4719@gmail.com>
2022-09-16 05:27:48 +0100

    update

--- main.tf
+++ main.tf
@@ -12,6 +12,21 @@ provider "aws" {
 }
 
 
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-git1"
+    key    = "global/s3/terraform-git1/states-files/tf.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "terraform-state-locking"
+    encrypt = true
+  }
+}
+
+
+
 resource "aws_key_pair" "deployer" {
   key_name   = "EMMANUEL-KEY"
   public_key = tls_private_key.rsa.public_key_openssh
@@ -28,6 +43,63 @@ resource "local_file" "TF-key" {
     content  = tls_private_key.rsa.private_key_pem
     filename = "tfkey"
 }
+
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "clarusway-cohort-10-team05"
+#   force_destroy = true
+
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+
+#   versioning {
+#     enabled = true
+#   }
+
+#   server_side_encryption_configuration {
+#     rule{
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+
+# #   tags = {
+# #     Name        = "My bucket"
+# #     Environment = "Dev"
+# #   }
+# # }
+
+# # resource "aws_s3_bucket_acl" "example" {
+# #   bucket = aws_s3_bucket.b.id
+# #   acl    = "private"
+# }
+
+resource "aws_dynamodb_table" "example" {
+  name             = "terraform-state-locking"
+  hash_key         = "LockID"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 resource "aws_instance" "master" {
   ami        = "ami-0960ab670c8bb45f3"
